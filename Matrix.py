d7a66353d08423ecdaae789f2b0dac8b20c932b9
Tristan <tristanp93@googlemail.com>
2019-03-26 21:15:38 +0100

    Some cleaning up

--- Matrix.py
+++ Matrix.py
@@ -1,14 +1,13 @@
 import time, random, sys, argparse
 import numpy as np
-import glc
+import includes.opc as opc
 from random import randint
-
 
 class Matrix(object):
 	def __init__(self, width, height):
 		self.w = width
 		self.h = height
-		self.connection = glc.InitOPC()
+		self.connection = self.initOPC()
 
 		self.mat = np.zeros((self.w,self.h,3))
 		
@@ -18,7 +17,7 @@ class Matrix(object):
 		self.mat[x,y,2] = b
 
 	def drawMatrix(self):
-		self.connection.put_pixels(glc.image2pixels(self.mat))
+		self.connection.put_pixels(self.image2pixels(self.mat))
 
 	def clearMatrix(self):
 		self.mat = np.zeros((self.w,self.h,3))
@@ -48,3 +47,62 @@ class Matrix(object):
 				self.mat[i][j] = [r,g,b]
 		self.drawMatrix()
 
+	def initOPC(self, url='localhost:7890'):
+		return opc.Client(url)
+		
+	def image2pixels(self, image):
+	    """
+	    imgage2pixels(image) convert an numpy rgb image (3D array) to the low level
+	    pixel (tuple) sequence for opc
+	    
+	    Parameters
+	    ----------
+	    image: 3D numpy array, rowfirst, i.e. row x cols x 3
+	    
+	    Returns
+	    -------
+	    a column-first reshaped and padded array of tuples to match opc format
+	    """
+	    # An image is layouted in first x, then y direction starting with the most 
+	    # top left index: 
+	    # 0,0 1,0 2,0 ...
+	    # 0,1 1,1 2,1 ...
+	    # 0,2 1,2 2,2 ...
+		# ... ... ... ... 
+		# 
+	    # In opc the pixels are adressed column-first and it seems to be fixed to
+	    # 512 pixels in total (check JSON of fcserver?)
+	    # 0  64
+	    # 1  65
+	    # .. ..
+	    # 63 127
+	    pixels = [ (0,0,0) ] * 512
+	    imgS = image.shape
+	    for xInd in range(imgS[0]):
+	        for yInd in range(imgS[1]):
+	            index = xInd*8 + yInd if xInd % 2 == 0 else (xInd*8)+(7-yInd)
+	            pixels[index] = tuple(image[xInd,yInd,0:3])
+	    return pixels
+	
+	def destOPC(self,c,img,fadetime = .66):
+		"""
+		destOPC() Destructor for the client of OPC,
+				ensures, that all pixels are faded out and are hence off when stopping
+				the programm.
+			
+			Parameters
+			----------
+			c   : the OPC client
+			img : the image to fade out
+			fadetime : the speed with wich to fade out
+		"""
+		# flash!
+		img_BLACK = np.zeros( (8,16,3) )
+		c.put_pixels(self.image2pixels(np.mean(img).astype(int)-img_BLACK))
+		time.sleep(fadetime/10)
+		c.put_pixels(self.image2pixels(img))
+		for i in range(np.round(np.max(img)).astype(int),0,-1):
+			c_img = np.round(img/np.max(img)*i)
+			c.put_pixels(self.image2pixels(c_img))
+			time.sleep(fadetime*9/(10*np.max(img)))
+		print('Ciaobelko. LEDGo out.')
--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 A JS / Python control interface for a fadecandy-driven 8x8 LED matrix
 
 [![LED GO](https://img.youtube.com/vi/OWNRfNsfjfw/0.jpg)](https://www.youtube.com/watch?v=OWNRfNsfjfw)
+
+
+
+## TODO Brainstorming
+
+- [ ] Connection status to fadecandy server / LED matrix
+- [ ] Some default color palettes
+- [ ] Drawing by hold-and-pull
+- [ ] Activation of animations
+- [ ] Selfmade animations by drawing multiple keyframes
+- [ ] Sliding text to matrix
